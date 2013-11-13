1612a60a2dd0544c77f34b2f38b354776bce3054
brunoksato <bruno.sato@live.com>
2013-11-13 13:45:02 -0200

    Create project in github

---
+++ AdvocaciaGeraldo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvocaciaGeraldo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "A Empresa";
+
+            return View();
+        }
+
+        public ActionResult Atuacao()
+        {
+            ViewBag.Message = "Áreas de atuação";
+
+            return View();
+        }
+
+        public ActionResult Equipe()
+        {
+            ViewBag.Message = "Equipe Profissional";
+
+            return View();
+        }
+
+        public ActionResult Processo()
+        {
+            ViewBag.Message = "Processos";
+
+            return View();
+        }
+
+        public ActionResult Acordo()
+        {
+            ViewBag.Message = "Acordos";
+
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            ViewBag.Message = "Contatos";
+
+            return View();
+        }
+    }
+}
